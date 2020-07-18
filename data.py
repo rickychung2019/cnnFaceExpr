3fdd8cf0b44be5c1fa8ae3cc609d5cff7170313a
rickychung2019 <57457425+rickychung2019@users.noreply.github.com>
2020-07-18 23:34:56 +0800

    Update data.py

--- data.py
+++ data.py
@@ -8,8 +8,10 @@ def getData(file):
 def load():
     n,train=getData('train.csv')
     index=int(3*n/4)
-    y_train=train['emotion'].iloc[0:index].to_numpy()
-    x_train=[np.reshape(np.asarray(tmp.split(),dtype=float),(-1,48)) for tmp in train[' pixels'].iloc[0:index]]
-    y_test=train['emotion'].iloc[index:-1].to_numpy()
-    x_test=[np.reshape(np.asarray(tmp.split(),dtype=float),(-1,48)) for tmp in train[' pixels'].iloc[index:-1]]
+
+    y_train=tf.keras.utils.to_categorical(train['emotion'].iloc[0:index].to_numpy())
+    x_train=np.asarray([np.reshape(np.asarray(tmp.split(),dtype=float),(-1,48)) for tmp in train[' pixels'].iloc[0:index]]).reshape(index,48,48,1)
+    y_test=tf.keras.utils.to_categorical(train['emotion'].iloc[index:n+1].to_numpy())
+    x_test=np.asarray([np.reshape(np.asarray(tmp.split(),dtype=float),(-1,48)) for tmp in train[' pixels'].iloc[index:n+1]]).reshape(n-index,48,48,1)
+
     return x_train, y_train, x_test, y_test
